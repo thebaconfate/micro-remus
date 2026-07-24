48eac936f9050f2e63e4f1fdbb63ef37211eb8d2
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-07-24 03:17:04 +0200

    ported location.rs

---
+++ src/micro-remus/location.c
@@ -0,0 +1,7 @@
+#include "location.h"
+#include "abstractions.h"
+#include "value.h"
+
+// TODO: implement this
+bool location_fetch(const Location *loc, DeploymentId deployment_id,
+                    const struct Remus *remus, Value *out_val);
---
+++ src/micro-remus/location.h
@@ -0,0 +1,36 @@
+#ifndef REMUS_LOCATION_H
+#define REMUS_LOCATION_H
+
+#include "types.h"
+#include "value.h"
+#include <stdbool.h>
+
+struct Remus;
+
+typedef enum {
+  LOC_D,
+  LOC_R,
+  LOC_I,
+  LOC_O,
+} LocationType;
+
+typedef struct Location {
+  LocationType type;
+  Number index;
+} Location;
+
+/**
+ * @brief Fetches a Value from Remus memory based on location type and index.
+ *
+ * @param loc The location descriptor (D, R, I, or O memory).
+ * @param deployment_id The ID of the deployment to query.
+ * @param remus Pointer to the global/context Remus engine instance.
+ * @param out_val Pointer to a Value struct where the result will be written.
+ *
+ * @return true if a value was found and written to out_val, false otherwise
+ * (Option::None).
+ */
+bool location_fetch(const Location *loc, DeploymentId deployment_id,
+                    const struct Remus *remus, Value *out_val);
+
+#endif
