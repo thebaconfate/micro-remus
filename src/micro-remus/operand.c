78633ad9e58b56ff872ae30f983575f62ed2e76e
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-07-27 01:02:29 +0200

    added boilerplate

--- src/micro-remus/operand.c
+++ src/micro-remus/operand.c
@@ -1,1 +1,7 @@
 #include "operand.h"
+#include "option.h"
+
+ValueOption operand_fetch(Operand *operand, DeploymentId deployment_id,
+                          struct Remus *remus) {
+  return (ValueOption){};
+}
